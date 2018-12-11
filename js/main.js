0935f4eb22f92e3c8600a245104c327d473eb165
stanislavcernovolod <johnstas977@gmail.com>
2018-12-11 18:07:30 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// var header = document.querySelector('header');
+// var imageWrapper = document.querySelector('.img-wrapper');
+//
+// setInterval(function(){
+//     header.className = 'first';
+//
+// },4000);
+// setInterval(function() {
+//     if (    header.className === 'first') {
+//         header.className = 'second';
+//     }
+// },8000);
+//
+// imageWrapper.onclick = function() {
+//     imageWrapper.classList.remove('img-wrapper');
+//     imageWrapper.classList = 'active';
+// };
+
+var europeGallery = document.querySelectorAll('.europe-img');
+
+
+(function showModal () {
+
+    for(var i = 0; i < europeGallery.length; i++) {
+        europeGallery[i].addEventListener('click', function () {
+
+            }, false);
+
+
+    }
+})();
+
+
